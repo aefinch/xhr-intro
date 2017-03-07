092adc1d71b3b2da1c7574896c29ad13e0646a40
Andrea Finch <aefinch@vt.edu>
2017-03-06 19:58:01 -0600

    event listeners added and link to json

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("let's learn some Bootstrap!")
+var myRequest = new XMLHttpRequest();
+function executeThisCodeAfterFileLoaded(){
+	console.log("IT WORKED!!!");
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("boooooo");
+}
+
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET","dinosaurs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
